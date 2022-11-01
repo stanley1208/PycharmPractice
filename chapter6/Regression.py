0a6fc25a408f14d1929101dbb8d25bd105037d68
A8250092 <stanley.mei08@gmail.com>
2022-11-01 14:03:07 +0800

    update

--- chapter6/Regression.py
+++ chapter6/Regression.py
@@ -96,3 +96,30 @@ export_graphviz(
 )
 Source.from_file(os.path.join(IMAGES_PATH,"regression_tree.dot"))
 
+tree_reg1=DecisionTreeRegressor(random_state=42)
+tree_reg2=DecisionTreeRegressor(random_state=42,min_samples_leaf=10)
+tree_reg1.fit(X,y)
+tree_reg2.fit(X,y)
+
+x1=np.linspace(0,1,500).reshape(-1,1)
+y_pred1=tree_reg1.predict(x1)
+y_pred2=tree_reg2.predict(x1)
+
+fig,axes=plt.subplots(ncols=2,figsize=(10,8),sharey=True)
+plt.sca(axes[0])
+plt.plot(X,y,"b.")
+plt.plot(x1,y_pred1,"r.-",linewidth=2,label=r"$\hat{y}$")
+plt.axis([0,1,-0.2,1.1])
+plt.xlabel("$x_1$",fontsize=18)
+plt.ylabel("$y$",fontsize=18,rotation=0)
+plt.legend(loc="upper center",fontsize=15)
+plt.title("No restrictions",fontsize=14)
+
+plt.sca(axes[1])
+plt.plot(X,y,"b.")
+plt.plot(x1,y_pred2,"r.-",linewidth=2,label=r"$\hat{y}$")
+plt.axis([0,1,-0.2,1.1])
+plt.xlabel("$x_1$",fontsize=18)
+plt.title("min_sample_leaf={}".format(tree_reg2.min_samples_leaf),fontsize=14)
+
+plt.show()
